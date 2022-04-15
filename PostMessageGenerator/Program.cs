6eb126b0238394d5c8f6a2f8e8779554ef7b5d2f
nirzaf <mfmfazrin1986@gmail.com>
2022-04-15 12:52:29 +0530

    Add project files.

---
+++ PostMessageGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+List<OutboundMessage> messages = new();
+for(int i = 0; i < 3; i++) 
+{
+    OutboundMessage message = new();
+    message.ProviderName = "email-postmark";
+    message.Properties = new Dictionary<string, string>()
+    {
+        { "From", Faker.Internet.FreeEmail() },
+        { "HtmlBody", Faker.Address.StreetAddress() },
+        { "ReplyTo", Faker.Internet.FreeEmail() },
+        { "Subject", Faker.Country.Name() },
+        { "To", Faker.Internet.Email() }
+    };
+    messages.Add(message);
+}
+
+var data = JsonSerializer.Serialize(messages);
+
+Console.WriteLine(data);
+
+//foreach (var mes in message.Properties)
+//{
+//    Console.WriteLine(mes.Key + "   " + mes.Value);
+//}
+
+//Console.ReadLine();
+public class OutboundMessage 
+{
+    public string? ProviderName { get; set; }
+    public Dictionary<string, string>? Properties { get; set; }
+}
